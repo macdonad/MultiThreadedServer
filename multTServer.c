2b57becb17694d820ab6b1d241648f8a852b7201
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-19 14:42:13 -0400

    header and doc

--- multTServer.c
+++ multTServer.c
@@ -1,49 +1,93 @@
+// multTServer.c
+
 #include <pthread.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+#include "multTServer.h"
 
 
-#define INPUT_BUFFER_SIZE 1024
+/* **************************************************************
+ * Types
+ * ************************************************************** */
+
+
+/**
+ * File worker struct type.
+ * 
+ * ID: Cooresponding thread ID
+ * FILENAME: Retrieval job file
+ * ACTIVE: False if the retrieval completed or failed
+ * PEER: Pointer to the other most recent worker created.
+ */
+struct _file_worker_t 
+{
+  pthread_t id;
+  char* filename;
+  bool active;
+  struct _file_worker_t* peer;
+};
+
+
+/**
+ * File worker type.
+ */
+typedef struct _file_worker_t file_worker_t;
+
+
+/* **************************************************************
+ * Globals
+ * ************************************************************** */
+
 
 /**
  * Input buffer
  */
 char* input_buffer;
 
+
 /**
  * Global access time
  */
+/* TODO */
+
 
 /**
  * Global request counter
  */
 int total_requests;
 
-/**
- * Handle a C-c
- */
-void handle_sigint(int sig);
 
 /**
- * Retrieve file
+ * The most recently started worker.
  */
-void* worker_function(void* arg);
+file_worker_t* last_worker;
 
+
+/**
+ * Main
+ */
 int
 main()
 {
   signal(SIGINT, handle_sigint);
+  input_buffer = ARRLOC(char*,  INPUT_BUFFER_SIZE);
   while(1) {
-    input_buffer = malloc(sizeof(char*) * INPUT_BUFFER_SIZE);
     fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
+    char* newline_pos = strchr(input_buffer, '\n');
+    if (newline_pos != NULL) *newline_pos = '\0';
     int filename_size = strlen(input_buffer);
-    char* filename = malloc(sizeof(char*) * filename_size);
-    memcpy(filename, input_buffer, filename_size);
-    pthread_t worker_thread;
-    pthread_create(&worker_thread, NULL, worker_function, filename);
+    file_worker_t* worker = malloc(sizeof(*worker));
+    worker->filename = ARRLOC(char*, filename_size);
+    worker->active = true;
+    memcpy(worker->filename, input_buffer, filename_size);
+    worker->peer = last_worker;
+    pthread_create(&(worker->id), NULL, worker_function, worker);
+    last_worker = worker;
   }  
   return 0;
 }
@@ -51,23 +95,44 @@ main()
 
 /**
  * Handle a C-c
+ *
+ * On SIGINT, join all worker threads, print stats, and exit.
  */
 void
 handle_sigint(int sig) 
 {
-  printf("SIGNALLIN!\n");
+  printf("\nBeginning shutdown\n");
+  file_worker_t* worker = last_worker;
+  while (worker && worker->peer)
+    {
+      pthread_join(worker->id, (void*)NULL);
+      file_worker_t* current = worker;
+      worker = worker->peer;
+      free(current);
+    }
+  free(input_buffer);
   exit(0);
 }
 
+
+/**
+ * Worker function
+ * Each worker will retrieve a file then report completion
+ *
+ * ARG: a pointer to this file_worker_t
+ */
 void* 
 worker_function(void* arg) 
 {
   total_requests++;
   /* insert time crap here */
-  char* filename = (char*) arg;
+  file_worker_t* this = (file_worker_t*) arg;
   int rand_val = rand() % 10;
   sleep((rand_val >= 2) ? 1 : 7 + (rand() % 4));
-  printf("Retrieved: %s\n", filename);
-  free(filename);
+  this->active = false;
+  printf("Thread: [%u] retrieved: %s\n", 
+	 // this is silly
+	 (unsigned int)(uintptr_t)(this->id), this->filename);
+  free(this->filename);
   return NULL;
 }
