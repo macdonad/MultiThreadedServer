f43cabdb58a8c6d6c71bf887d27dfe30b116589c
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-19 11:59:29 -0400

    fgets!

--- multTServer.c
+++ multTServer.c
@@ -5,8 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+#define INPUT_BUFFER_SIZE 1024
+
+/**
+ * Input buffer
+ */
+char* input_buffer;
+
+
 int
 main()
 {
+  input_buffer = malloc(sizeof(char*) * INPUT_BUFFER_SIZE);
+  fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
   return 0;
 }
